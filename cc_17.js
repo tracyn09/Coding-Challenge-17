0792c90ea568016727d1a7fc050fc377c8671cd4
tracyn09 <tracyn0099@gmail.com>
2025-03-29 08:04:32 -0400

    Task 2–Created Sales Rep Class

--- cc_17.js
+++ cc_17.js
@@ -29,4 +29,48 @@
     console.log(`Total Spent: $${customer0.getTotalSpent()}`)
 
 
- 
+    
+//Task 2
+    //Creating salesRep class
+    class SalesRep {
+        constructor(name) {
+            this.name = name
+            this.clients = []
+        }
+        
+    //Adding customers to list
+    addClient(customer) {
+        this.clients.push(customer)
+    }
+
+    //Find client by name and return total spent
+    getClientTotal(name) {
+        const client = this.clients.find(c => c.name === name)
+        if (client) {
+            return client.getTotalSpent()
+        }
+        else { 
+            return "Null"
+        }
+    }
+}
+
+    //Adding new customer
+    const customer1 = new Customer ("John", "jrold@icloud.com")     
+    customer1.addPurchase(50)
+
+    //Logging sales rep's clients and total spent (Task 3)
+    const SalesRep0 = new SalesRep ("Annie")                        //Creating salesRep
+    SalesRep0.addClient(customer0)                                  
+    SalesRep0.addClient(customer1)                                  //Adding new client to salesRep
+
+    //Logging sales rep and list of clients
+    console.log(`${SalesRep0.name}'s Clients:`)                   
+    SalesRep0.clients.forEach(client => {
+        console.log(`${client.name}: $${client.getTotalSpent()}`)
+    })
+
+    console.log(`Total spent by John: $${SalesRep0.getClientTotal("John")}`)     //Logging total spent by customer1
+
+
+
